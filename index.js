fc73b648d4c7521fc984f78ed59eda2a90c69337
gatewaa <112648401+gatewaa@users.noreply.github.com>
2022-11-17 13:00:05 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let homeEl = document.getElementById("home-el")
 let guestEl = document.getElementById("guest-el")
+let winnerEl = document.getElementById("winner-el")
 let homeScore = 0
 let guestScore = 0
 
@@ -15,6 +16,11 @@ function homeThree(){
     homeScore += 3
     homeEl.textContent=homeScore
 }
+function hm1(){
+    homeScore -=1
+    homeEl.textContent= homeScore
+}
+
 function guestOne(){
     guestScore += 1
     guestEl.textContent=guestScore
@@ -27,9 +33,25 @@ function guestThree(){
     guestScore += 3
     guestEl.textContent=guestScore
 }
+function gm1(){
+    guestScore -=1
+    guestEl.textContent= guestScore
+}
 function resetZero(){
+    detrmine()
     homeEl.textContent=0
     homeScore=0
     guestEl.textContent=0
     guestScore=0
+}
+function detrmine(){
+    if(homeScore < guestScore){
+        winnerEl.innerText = "Guest Wins"
+    }
+    if(homeScore > guestScore){
+    winnerEl.innerText = "Home Wins"
+    }
+    if(homeScore == guestScore){
+    winnerEl.innerText = "Draw"
+    }
 }
